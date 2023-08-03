894d3d3ebc470f3301f729aee646b3f3e9f70669
Nao Yonashiro <owan.orisano@gmail.com>
2023-08-03 11:51:03 +0900

    fix(engine/sqlite): added json_tree and json_each definitions
    fix #1830

--- internal/endtoend/testdata/table_function/postgresql/pgx/v4/query.sql
+++ internal/endtoend/testdata/table_function/postgresql/pgx/v4/query.sql
@@ -10,3 +10,17 @@ WHERE
 	AND jsonb_extract_path(transactions.data, '$.transaction.signatures[0]') @> to_jsonb(sqlc.arg('data')::text)
 	AND jsonb_extract_path(jsonb_extract_path(transactions.data, '$.transaction.message.accountKeys'), 'key') = to_jsonb(transactions.program_id)
 GROUP BY transactions.id;
+
+/* name: GetTransactionWithoutAlias :many */
+SELECT
+    json_extract(transactions.data, '$.transaction.signatures[0]'),
+    json_group_array(value)
+FROM
+    transactions,
+    json_each(json_extract(transactions.data, '$.transaction.message.instructions'))
+WHERE
+    transactions.program_id = $1
+    AND json_extract(transactions.data, '$.transaction.signatures[0]') > $2
+    AND json_extract(json_extract(transactions.data, '$.transaction.message.accountKeys'), '$[' || json_extract(value, '$.programIdIndex') || ']') = transactions.program_id
+GROUP BY transactions.id
+LIMIT $3;
